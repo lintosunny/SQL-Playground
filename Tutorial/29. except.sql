c46be47ac841f3be1e704d7c1184294310268027
Linto N S <119283666+lintosunny@users.noreply.github.com>
2024-05-29 13:47:44 +0530

    Update 29. except.sql

--- Tutorial/29. except.sql
+++ Tutorial/29. except.sql
@@ -39,3 +39,58 @@ FROM TableB;
 -- | 2  | Mary   | Female |
 -- | 3  | Steve  | Male   |
 -- +----+--------+--------+
+
+
+-- EXCEPT vs NOT IN
+-- 1. EXCEPT filters duplicates and returns distinct rows, whereas NOT IN doesn't filter the results
+-- 2. EXCEPT expects the same number of columns in both tables,
+--    NOT IN compares a single column in the outer query with a single column in a subquery.
+
+-- TableA
+-- +----+--------+--------+
+-- | Id | Name   | Gender |
+-- +----+--------+--------+
+-- | 1  | Mark   | Male   |
+-- | 2  | Mary   | Female |
+-- | 3  | Steve  | Male   |
+-- | 1  | Mark   | Male   |
+-- +----+--------+--------+
+
+-- TableB
+-- +----+--------+--------+
+-- | Id | Name   | Gender |
+-- +----+--------+--------+
+-- | 1  | Mary   | Female |
+-- | 2  | Steve  | Male   |
+-- +----+--------+--------+
+
+
+-- EXCEPT
+SELECT Id, Name, Gender
+FROM TableA
+EXCEPT
+SELECT Id, Name, Gender
+FROM TableB;
+
+-- Example output
+-- +----+--------+--------+
+-- | Id | Name   | Gender |
+-- +----+--------+--------+
+-- | 1  | Mark   | Male   |
+-- +----+--------+--------+
+
+
+-- NOT IN 
+SELECT Id, Name, Gender
+FROM TableA
+WHERE Id NOT IN (
+  SELECT Id
+  FROM TableB);
+
+-- Example output
+-- +----+--------+--------+
+-- | Id | Name   | Gender |
+-- +----+--------+--------+
+-- | 1  | Mark   | Male   |
+-- | 1  | Mark   | Male   |
+-- +----+--------+--------+
