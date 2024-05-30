ebc91f2c327b9724cfbd0d3895adb210abe4d877
Linto N S <119283666+lintosunny@users.noreply.github.com>
2024-05-30 12:50:01 +0530

    Create --wip--most asked join question.sql

---
+++ Coding Questions/Medium/--wip--most asked join question.sql
@@ -0,0 +1,4 @@
+-- There are two tables, first table has 5 records and second table has 10 records.
+-- assume any values in each tables
+-- how many max and min records possible in case of inner, left, right, and full outer joins.
+
